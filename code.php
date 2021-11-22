71920bcbeb2679849a419ec2f12ea2ddb84d28bc
Chrisnumber49 <h28925847@gmail.com>
2021-11-22 11:34:14 +0800

    PHP MySQL Employee Management App

--- code.php
+++ code.php
@@ -1,17 +1,17 @@
 <?php
     session_start();
 
-    //Get Heroku ClearDB connection information
-    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $cleardb_server = $cleardb_url["host"];
-    $cleardb_username = $cleardb_url["user"];
-    $cleardb_password = $cleardb_url["pass"];
-    $cleardb_db = substr($cleardb_url["path"],1);
-    $active_group = 'default';
-    $query_builder = TRUE;
-    // Connect to DB
-    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-    // $conn = mysqli_connect("localhost", "root", "", "crud");
+    // //Get Heroku ClearDB connection information
+    // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    // $cleardb_server = $cleardb_url["host"];
+    // $cleardb_username = $cleardb_url["user"];
+    // $cleardb_password = $cleardb_url["pass"];
+    // $cleardb_db = substr($cleardb_url["path"],1);
+    // $active_group = 'default';
+    // $query_builder = TRUE;
+    // // Connect to DB
+    // $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $conn = mysqli_connect("localhost", "root", "", "crud");
 
     // if press create button and method is POST
     if(isset($_POST['create'])){
